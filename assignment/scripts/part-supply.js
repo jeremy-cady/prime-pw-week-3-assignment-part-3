f6cfd6b5a8e69e63c489563d5216340233657ac9
jeremy-cady <jeremy.cady@outlook.com>
2021-11-14 14:44:00 -0600

    up through 7

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -60,6 +60,19 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
+for (change of supplyChanges) {
+    console.log('Supply change:', change);
+      if (change>0) {
+        console.log(`Added ${change} parts.`);
+      } //end if
+      else if (change===0) {
+        console.log('No change.');
+      } //end else if
+      else {
+        console.log(`Removed ${change} parts.`);
+      } //end else
+  } //end for
+
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
 
